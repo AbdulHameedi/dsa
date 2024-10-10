f2043ae00ebab297dbebccdd2ef121ff5126ed8d
AbdulHameedi <sanilinity507@gmail.com>
2024-10-10 13:27:05 +0100

    random cmd

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,18 @@
+// let firstSequence = [0,1]
+
+// function fibonacci(){
+
+//     // for(let i=firstSequence; i<firstSequence.length; i++){
+
+//     // }
+// } 
+
+
+// fibonacci(2)
+
+const cars = ['Volvo', 'Suzuki', 'Camry', 'Keke']
+let text = ''
+for(let i=0;i<cars.length;i++){
+    text =+ cars[i] + "<br/>"
+}
+console.log(text)
---
+++ random/random.js
@@ -0,0 +1,6 @@
+const cars = ['Volvo', 'Suzuki', 'Camry', 'Keke']
+let text = " "
+for(let i=0;i<cars.length;i++){
+    text += cars[i] + " "
+}
+console.log(text)
